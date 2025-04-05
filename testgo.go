9042faca3f9c45d4998e6d5c071672b820029dab
Clark1945 <dmc950213@gmail.com>
2025-04-05 22:46:18 +0800

    test2

--- testgo.go
+++ testgo.go
@@ -1,29 +1,29 @@
 package main
 
-// func main() {
-// 	firstRoutine := make(chan string) //宣告給第1個執行序的channel
-// 	secRoutine := make(chan string)   //宣告給第2個執行序的channel
-// 	rand.Seed(time.Now().UnixNano())
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-// 	go func() {
-// 		r := rand.Intn(100)
-// 		time.Sleep(time.Microsecond * time.Duration(r)) //隨機等待 0~100 ms
-// 		firstRoutine <- "first goroutine"
-// 	}()
-// 	go func() {
-// 		r := rand.Intn(100)
-// 		time.Sleep(time.Microsecond * time.Duration(r)) //隨機等待 0~100 ms
-// 		secRoutine <- "Sec goroutine"
-// 	}()
-// 	select { // select多路複用 select 會阻塞等待直到有任一個 case 的 channel 收到資料。
-// 	case f := <-firstRoutine: //第1個執行序先執行後所要做的動作
-// 		fmt.Println(f)
-// 		return
-// 	case s := <-secRoutine: // 第2個執行序先執行後所要做的動作
-// 		fmt.Println(s)
-// 		return
-// 	}
-// }
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	result := make(chan string)
+
+	// 建立 context，1 秒 timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel() // 確保最後釋放資源
+
+	go doWork(ctx, result)
+
+	select {
+	case res := <-result:
+		fmt.Println(res)
+	case <-ctx.Done(): // 有兩個地方監聽ctx，感覺寫得不好
+		fmt.Println("❌ 超時沒收到結果：", ctx.Err())
+	}
+}
 
 // func typicalChan() {
 // 	// 宣告channel make(chan 型態 <容量>)
@@ -94,3 +94,39 @@ package main
 // 	wg.Add(2) //記數器+2
 // 	wg.Wait() //等待計數器歸零
 // }
+
+// func doTheFirstThread() {
+// 	firstRoutine := make(chan string) //宣告給第1個執行序的channel
+// 	secRoutine := make(chan string)   //宣告給第2個執行序的channel
+// 	rand.Seed(time.Now().UnixNano())
+
+// 	go func() {
+// 		r := rand.Intn(100)
+// 		time.Sleep(time.Microsecond * time.Duration(r)) //隨機等待 0~100 ms
+// 		firstRoutine <- "first goroutine"
+// 	}()
+// 	go func() {
+// 		r := rand.Intn(100)
+// 		time.Sleep(time.Microsecond * time.Duration(r)) //隨機等待 0~100 ms
+// 		secRoutine <- "Sec goroutine"
+// 	}()
+// 	select { // select多路複用 select 會阻塞等待直到有任一個 case 的 channel 收到資料。
+// 	case f := <-firstRoutine: //第1個執行序先執行後所要做的動作
+// 		fmt.Println(f)
+// 		return
+// 	case s := <-secRoutine: // 第2個執行序先執行後所要做的動作
+// 		fmt.Println(s)
+// 		return
+// 	}
+// }
+
+func doWork(ctx context.Context, result chan<- string) {
+	// 模擬一個耗時不確定的工作
+	r := rand.Intn(3000) // 0~2999 毫秒
+	select {
+	case <-time.After(time.Duration(r) * time.Millisecond): // 延遲
+		result <- fmt.Sprintf("✅ 工作完成於 %d 毫秒", r)
+	case <-ctx.Done():
+		fmt.Println("⛔ 工作被取消，原因：", ctx.Err()) // context timeout 觸發
+	}
+}
